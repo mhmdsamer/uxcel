17a6ed27d69e6eba270fb134606cde41ceeedf6c
mhmdsamer <160267540+mhmdsamer@users.noreply.github.com>
2024-04-06 17:16:04 +0300

    Add files via upload

---
+++ uxcel/index.js
@@ -0,0 +1,21 @@
+function handleClick (){
+    var imageSrc1 = "images/2023-07-31 (15).png"
+    document.getElementsByClassName("img-1")[0].setAttribute("src" , imageSrc1);
+}
+document.getElementsByClassName("b-1")[0].addEventListener("click" , handleClick);
+
+function handleClick1 (){
+    var imageSrc1 = "images/2023-07-31 (18).png"
+    document.getElementsByClassName("img-1")[0].setAttribute("src" , imageSrc1);
+}
+document.getElementsByClassName("b-2")[0].addEventListener("click" , handleClick1);
+
+
+function handleClick2 (){
+    var imageSrc1 = "images/2023-07-31 (19).png"
+    document.getElementsByClassName("img-1")[0].setAttribute("src" , imageSrc1);
+}
+document.getElementsByClassName("b-3")[0].addEventListener("click" , handleClick2);
+
+
+
